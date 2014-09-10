af0d41d98582120f43514fdbdc9a1f935c10655e
Tom Wayson <tom@tomwayson.com>
2014-09-09 23:11:40 -0700

    using grunt for Dojo build
    removed build scripts
    resolves #2

--- gruntfile.js
+++ gruntfile.js
@@ -9,10 +9,74 @@ module.exports = function (grunt) {
           beautify: true
         }
       }
+    },
+    clean: ['dist'],
+    dojo: {
+      dist: {
+        options: {
+          profile: 'profiles/app.profile.js', // Profile for build
+        }
+      },
+      options: {
+        dojo: 'src/dojo/dojo.js', // Path to dojo.js file in dojo source
+        load: 'build', // Optional: Utility to bootstrap (Default: 'build')
+        // profiles: [], // Optional: Array of Profiles for build
+        // appConfigFile: '', // Optional: Config file for dojox/app
+        // package: '', // Optional: Location to search package.json (Default: nothing)
+        // packages: [], // Optional: Array of locations of package.json (Default: nothing)
+        // require: '', // Optional: Module to require for the build (Default: nothing)
+        // requires: [], // Optional: Array of modules to require for the build (Default: nothing)
+        releaseDir: '../dist', // Optional: release dir rel to basePath (Default: 'release')
+        cwd: './', // Directory to execute build within
+        // dojoConfig: '', // Optional: Location of dojoConfig (Default: null),
+        // Optional: Base Path to pass at the command line
+        // Takes precedence over other basePaths
+        // Default: null
+        basePath: './src'
+      }
+    },
+    'string-replace': {
+      index: {
+        src: './src/index.html',
+        dest: './dist/index.html',
+        options: {
+          replacements: [
+            // remove isDeubug
+            {
+              pattern: /isDebug: *true,/,
+              replacement: ''
+            },
+            // strip js comments
+            {
+              pattern: /\s+\/\/.*$/gm,
+              replacement: ''
+            },
+            // replace newlines w/ whitespace
+            {
+              pattern: /\n/g,
+              replacement: ' '
+            },
+            // strip html comments
+            {
+              pattern: /<!--[\s\S]*?-->/g,
+              replacement: ''
+            },
+            // collapse whitespace
+            {
+              pattern: /\s+/g,
+              replacement: ' '
+            }
+          ]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-esri-slurp');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-dojo');
+  grunt.loadNpmTasks('grunt-string-replace');
 
   grunt.registerTask('slurp', ['esri_slurp:dev']);
+  grunt.registerTask('build', ['clean', 'dojo', 'string-replace']);
 };
