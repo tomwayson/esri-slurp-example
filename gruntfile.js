39f9eb57a422d37c38cd7c7bec84d32256cdee14
arnaud ferrand <arferrand@gmail.com>
2015-03-19 17:34:21 +0100

    update to slurp 1.4.2 and JSAPI 3.13

--- gruntfile.js
+++ gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     // and where to put it
     esri_slurp: {
       options: {
-        version: '3.12'
+        version: '3.13'
       },
       dev: {
         options: {
